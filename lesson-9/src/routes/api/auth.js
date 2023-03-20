9a89bebe649239fb4ffd05624f287f99f5aa9120
Roman Boiko <boykoroman94@gmail.com>
2023-03-20 17:02:36 +0200

    init lesson 9

---
+++ lesson-9/src/routes/api/auth.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const { ctrlWrapper } = require("../../helpers")
+const authController = require("../../controllers/authController")
+const { validateBody, auth } = require("../../middlewares")
+const { registerSchema, loginSchema } = require("../../schemas/auth")
+
+const router = express.Router()
+
+router.post(
+  "/register",
+  validateBody(registerSchema),
+  ctrlWrapper(authController.register)
+)
+
+router.post(
+  "/login",
+  validateBody(loginSchema),
+  ctrlWrapper(authController.login)
+)
+
+router.get("/logout", auth, ctrlWrapper(authController.logout))
+
+module.exports = router
---
+++ lesson-9/src/schemas/auth/index.js
@@ -0,0 +1,21 @@
+const Joi = require("joi")
+
+const registerSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string()
+    .pattern(/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/)
+    .required(),
+  password: Joi.string().min(6).required(),
+})
+
+const loginSchema = Joi.object({
+  email: Joi.string()
+    .pattern(/[a-z0-9]+@[a-z]+\.[a-z]{2,3}/)
+    .required(),
+  password: Joi.string().min(6).required(),
+})
+
+module.exports = {
+  registerSchema,
+  loginSchema,
+}
